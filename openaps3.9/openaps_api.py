50645836a02486a86186bd34d5f9a1a7a00748a9
srini-imperial-sg <s.kolumamnandakumar@imperial.ac.uk>
2025-07-02 07:16:15 +0000

    API fix

--- openaps3.9/openaps_api.py
+++ openaps3.9/openaps_api.py
@@ -7,6 +7,7 @@ import time
 from subprocess import call
 from collections import namedtuple
 from datetime import datetime, timezone
+import datetime as dt
 Action = namedtuple('ctrller_action', ['basal', 'bolus'])
 
 logger = logging.getLogger(__name__)
@@ -28,18 +29,30 @@ def policy():
     observations = request.get_json()
     glucose = observations.get('CGM', 120)
     cho = observations.get('CHO', 0)
+    ind = observations.get('index', 0)
     print(f"Glucose: {glucose}, CHO: {cho}")
     data_to_prepend = data[0].copy()
     data_to_prepend["glucose"] = glucose
-    data_to_prepend["date"] = int(time.time().to())*1000+(1)*1*10*10
+    data_to_prepend["date"] = int(time.time() * 1000) + ind * 5 * 60 * 1000
     data_to_prepend["dateString"] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S")
+    data_to_prepend["noise"] = 0
+    data_to_prepend["short_avgdelta"] = 0
+    data_to_prepend["long_avgdelta"] = 0
+    data_to_prepend["delta"] = 0
 
     print("data_to_prepend", data_to_prepend)
     data.insert(0, data_to_prepend)
     
    
-    with open("/home/srini/ts/OpenAPS-Glucosym-3.9/openaps3.9/monitor/glucose.json", 'w') as outfile:
+    with open("./monitor/glucose.json", 'w') as outfile:
         json.dump(data, outfile, indent=4)
+    
+    tz = int(-(time.altzone if (time.daylight and time.localtime().tm_isdst > 0) else time.timezone)/3600) ## Time zone offset
+    current_timestamp = dt.datetime.fromtimestamp(time.time()+0*60*60+ind*5*60).strftime('%Y-%m-%dT%H:%M:%S') ## Local time
+    current_timestamp = current_timestamp + ("-" if tz<0 else "+") + str(abs(tz)).zfill(2) + ":00" ## Time zone offset appended
+
+    with open('monitor/clock.json','w') as update_clock:
+        json.dump(current_timestamp, update_clock)
    
     call(["openaps", "report", "invoke", "settings/profile.json"])
     call(["openaps", "report", "invoke", "monitor/iob.json"])
